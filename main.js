b28ca105c4dc9ad13189446bbe88a47e7555d77b
anniep8911 <pjatopazz@gmail.com>
2024-11-06 10:03:17 +0900

    fix: 윈도우 입력 키 값 up,down 스테이트 구분

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ const { GlobalKeyboardListener } = require("node-global-key-listener");
 
 let win;
 const keyboardListener = new GlobalKeyboardListener();
+const keyState = new Map();
+
 
 function createWindow() {
     win = new BrowserWindow({
-        width: 400,
-        height: 300,
+        width: 1000,
+        height: 600,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
@@ -22,9 +24,25 @@ function createWindow() {
 keyboardListener.addListener((event) => {
     // 이벤트를 Electron 윈도우에 전달
     if (win) {
+        // console.log(event._raw);
+        let state =  event._raw.split(',');
+        event.rawKey.state=state[1];
         win.webContents.send('key-event', event.rawKey); // 키 값을 UI에 전송
     }
-});
-akjaskjdkj112323434546567678768789890
+     // 키가 눌렸을 때 (keydown)
+    //  if (event.eventType === 'keydown' && !keyState.has(event.rawKey)) {
+    //     keyState.set(event.rawKey, 'down'); // 키 상태를 'down'으로 설정
+    //     if (win) {
+    //         win.webContents.send('key-event', { type: 'keydown', key: event.rawKey });
+    //     }
+    // }s
 
+    // // 키가 떼졌을 때 (keyup)
+    // if (event.eventType === 'keyup' && keyState.has(event.rawKey) && keyState.get(event.rawKey) === 'down') {
+    //     keyState.set(event.rawKey, 'up'); // 키 상태를 'up'으로 설정
+    //     if (win) {
+    //         win.webContents.send('key-event', { type: 'keyup', key: event.rawKey });
+    //     }
+    // }
+});
 app.whenReady().then(createWindow);
